5a07a4c0b755a257d7e93616ead4b077933e5473
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2018-03-19 10:13:18 +0800

    scipy

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Hello World app for running Python apps on Bluemix
+BankDepositAssistor
 """
 
 # Always prefer setuptools over distutils
@@ -15,10 +15,10 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='python-hello-world-flask',
+    name='BankDepositAssistor',
     version='1.0.0',
-    description='Hello World app for running Python apps on Bluemix',
+    description='BankDepositAssistor',
     long_description=long_description,
-    url='https://github.com/IBM-Bluemix/python-hello-world-flask',
+    url='https://github.com/zh0014in/BankDepositAssistor-Python',
     license='Apache-2.0'
 )
