e6232ec8941c3f0f5e9b794764e59a3bb2203042
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2018-03-19 20:08:36 +0800

    distribution plot

--- static/distributionPlots.js
+++ static/distributionPlots.js
@@ -25,13 +25,18 @@
                 series: {
                     argumentField: "key",
                     valueField: "value",
-                    name: "Month",
+                    name: "",
                     type: "bar",
-                    // color: '#ffaa66'
+                    visible: true
                 },
                 rotated: false,
                 size: {
                     width: 800
+                },
+                legend: {
+                    horizontalAlignment: "right",
+                    position: "inside",
+                    border: { visible: true }
                 },
                 onInitialized: function (e) {
                     vm.monthChart = e.component;
@@ -46,10 +51,10 @@
             });
             vm.countByColumn = countByColumn;
             function countByColumn(column) {
-                if(!vm.data){
+                if (!vm.data) {
                     return;
                 }
-                var groupByMonth = vm.data.groupBy(function (t) {
+                var groupByColumn = vm.data.groupBy(function (t) {
                     return t[column]
                 }).select(function (t) {
                     return {
@@ -57,8 +62,23 @@
                         value: t.length
                     }
                 });
-                console.log(groupByMonth);
-                vm.monthChart.option('dataSource', groupByMonth);
+                if (column === 'month') {
+                    var sorting = { 'jan': 0, 'feb': 1, 'mar': 2, 'apr': 3, 'may': 4, 'jun': 5, 'jul': 6, 'aug': 7, 'sep': 8, 'oct': 9, 'nov': 10, 'dec': 11 };
+                    // sort the data array
+                    groupByColumn.sort(function (a, b) {
+                        // sort based on the value in the monthNames object
+                        return sorting[a.key] - sorting[b.key];
+                    });
+                }else{
+                    groupByColumn.sort(function (a, b) {
+                        // sort based on the value in the monthNames object
+                        return a.key-b.key;
+                    });
+                }
+                console.log(groupByColumn);
+                vm.monthChart.option('series.name', column);
+                vm.monthChart.option('dataSource', groupByColumn);
+                // vm.monthChart.render();
                 vm.showChart = true;
             }
         }
