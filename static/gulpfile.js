bfe00fcf0a6c36573bff9e71a7605f966502ea8d
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2018-03-21 20:57:31 +0800

    1

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
+gulp.task('css', function(){
+    return gulp.src([
+        // 'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css'
+    ])
+    .pipe(concat('module.css'))
+    .pipe(gulp.dest('dist'));
+});
+
 // define tasks here
-gulp.task('default', function () {
+gulp.task('default', ['css'], function () {
     // run tasks here
     // set up watch handlers here
     return gulp.src([
             'js/*.js',
             'node_modules/sarsha-loading-spinner/loading-spinner.min.js',
+            // 'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
             './app.js',
             'src/*.js',
         ])
