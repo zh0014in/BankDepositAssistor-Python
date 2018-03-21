772088225d23e8f6bbe0c4b5013e82faca2e5d3a
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2018-03-21 16:51:49 +0800

    move npm

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -6,10 +6,10 @@ gulp.task('default', function () {
     // run tasks here
     // set up watch handlers here
     return gulp.src([
-            'static/js/*.js',
+            'js/*.js',
             'node_modules/sarsha-loading-spinner/loading-spinner.min.js',
-            'static/*.js',
-            'static/src/*.js',
+            './*.js',
+            'src/*.js',
         ])
         .pipe(concat('module.js'))
         .pipe(gulp.dest('static/dist'));
