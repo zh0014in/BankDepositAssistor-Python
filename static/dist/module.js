4546ec878c296b02ef71301c77a39e5d9d2e85ab
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2018-03-22 15:36:03 +0800

    select pkl

--- static/dist/module.js
+++ static/dist/module.js
@@ -259,7 +259,7 @@ class AntiXSS {
                 
             });
 
-            $scope.$on('fileloaded', function(event, args){
+            $scope.$on('fileloaded', function(event, args){ 
                 $scope.isTestFile = args.isTestFile;
                 vm.show = true;
             });
@@ -800,6 +800,66 @@ class AntiXSS {
 
             },
             controller: selectModelController,
+            controllerAs: 'vm'
+        }
+    }
+
+}());
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .component('selectPkl', selectPkl());
+
+
+    function selectPkl() {
+        selectPklController.$inject = ['$http', '$scope', '$rootScope']
+
+        function selectPklController($http, $scope, $rootScope) {
+            var vm = this;
+
+            vm.$onInit = init;
+
+            function init() {
+                $http.get('/getexistingmodels').then(function (response) {
+                    vm.pkls = Object.keys(response.data).reduce(function (r, e) {
+                        if (e.startsWith($rootScope.selectedModel)) r[e] = response.data[e];
+                        return r;
+                    }, {});
+                    vm.pklNames = Object.keys(vm.pkls);
+                    vm.selectBox.option('items', vm.pklNames);
+                });
+            }
+
+            $scope.$on('modelSelectionChanged', function () {
+                init();
+            });
+
+            $scope.selectedPkl = '';
+            vm.simple = {
+                items: [],
+                placeholder: 'select saved model',
+                bindingOptions: {
+                    value: "selectedPkl"
+                },
+                onSelectionChanged: function (e) {
+                    $rootScope.$broadcast('pklSelectionChanged', {
+                        pkl: e.selectedItem
+                    });
+                },
+                onInitialized: function (e) {
+                    vm.selectBox = e.component;
+                }
+            };
+        }
+
+        return {
+            templateUrl: "static/html/selectPkl.html",
+            bindings: {
+
+            },
+            controller: selectPklController,
             controllerAs: 'vm'
         }
     }
---
+++ static/src/selectPkl.js
@@ -0,0 +1,60 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .component('selectPkl', selectPkl());
+
+
+    function selectPkl() {
+        selectPklController.$inject = ['$http', '$scope', '$rootScope']
+
+        function selectPklController($http, $scope, $rootScope) {
+            var vm = this;
+
+            vm.$onInit = init;
+
+            function init() {
+                $http.get('/getexistingmodels').then(function (response) {
+                    vm.pkls = Object.keys(response.data).reduce(function (r, e) {
+                        if (e.startsWith($rootScope.selectedModel)) r[e] = response.data[e];
+                        return r;
+                    }, {});
+                    vm.pklNames = Object.keys(vm.pkls);
+                    vm.selectBox.option('items', vm.pklNames);
+                });
+            }
+
+            $scope.$on('modelSelectionChanged', function () {
+                init();
+            });
+
+            $scope.selectedPkl = '';
+            vm.simple = {
+                items: [],
+                placeholder: 'select saved model',
+                bindingOptions: {
+                    value: "selectedPkl"
+                },
+                onSelectionChanged: function (e) {
+                    $rootScope.$broadcast('pklSelectionChanged', {
+                        pkl: e.selectedItem
+                    });
+                },
+                onInitialized: function (e) {
+                    vm.selectBox = e.component;
+                }
+            };
+        }
+
+        return {
+            templateUrl: "static/html/selectPkl.html",
+            bindings: {
+
+            },
+            controller: selectPklController,
+            controllerAs: 'vm'
+        }
+    }
+
+}());
